4b027d930a53f6236de22868a0c804280b5bcc78
MaciejGrzelec <maciej.and.grzelec@gmail.com>
2026-07-22 18:02:25 +0200

    Config server

--- api-gateway/src/main/java/pl/clinicflow/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/pl/clinicflow/api_gateway/ApiGatewayApplication.java
@@ -2,12 +2,12 @@ package pl.clinicflow.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ApiGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
-
 }
