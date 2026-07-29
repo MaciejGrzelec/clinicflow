b1b564fd889b792a708df1c3244ec1b2e4516e0a
MaciejGrzelec <maciej.and.grzelec@gmail.com>
2026-07-29 18:18:15 +0200

    Changed css to use tailwind

--- frontend/clinic/src/App.tsx
+++ frontend/clinic/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Header></Header>
-      <div>clinic</div>
+      <h1 className="text-3xl font-bold underline">Hello world</h1>
       <Footer></Footer>
     </>
   )
